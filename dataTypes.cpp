47b0396857bd679b88e9285d4791ef296bfec16c
Purnima <purnimavats6789@gmail.com>
2024-09-13 11:07:09 +0530

    basic setup and datatypes

---
+++ dataTypes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int, long, long long, float, double
+
+    // string and getline
+    // strings will be in double quotes
+
+    // string str;
+    // cin >> str; // It will only take one word before space
+    // getline(cin, str); // It will take whole line
+    // cout << str;
+
+    // char
+    // single quotes are used for character
+    char ch;
+    // cin >> ch;
+    ch = 'a';
+    cout << ch;
+    return 0;
+}
---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cout << "Hey " << x;
+}
+
+// Ctrl + Shift + B : to run the code
+// Your cursor has to be on demo.cpp
+// Your default terminal has to be cmd. It won't work if your default terminal is powershell.
